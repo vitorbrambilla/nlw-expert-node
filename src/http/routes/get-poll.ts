caa63e4fd382f6df96bde55e15df4033d7964ad0
Vitor <vitor.reis@crmservices.com.br>
2024-02-08 14:03:42 -0300

    feat: redis and websocket connection

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify";
 import z from "zod";
 import { prisma } from "../../lib/prisma";
+import { redis } from "../../lib/redis";
 
 export async function getPoll(app: FastifyInstance) {
   app.get("/polls/:pollId", async (request, reply) => {
@@ -24,6 +25,34 @@ export async function getPoll(app: FastifyInstance) {
       },
     });
 
-    return reply.send({ poll });
+    if (!poll) {
+      return reply.status(400).send({ message: "Poll not found." });
+    }
+
+    const resut = await redis.zrange(pollId, 0, -1, "WITHSCORES");
+
+    const votes = resut.reduce((obj, line, index) => {
+      if (index % 2 === 0) {
+        const score = resut[index + 1];
+
+        Object.assign(obj, { [line]: Number(score) });
+      }
+
+      return obj;
+    }, {} as Record<string, number>);
+
+    console.log(votes);
+
+    return reply.send({
+      poll: {
+        id: poll.id,
+        title: poll.title,
+        options: poll.options.map((option) => ({
+          id: option.id,
+          title: option.title,
+          score: option.id in votes ? votes[option.id] : 0,
+        })),
+      },
+    });
   });
 }
